c1aa39df4098c40cb150e76061d1889b8988c58f
2kmountains <76716532+7beggars@users.noreply.github.com>
2022-12-30 01:39:20 -0300

    Add files via upload

---
+++ data/networks.js
@@ -0,0 +1,81 @@
+var networks = {"Network": {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.9.1",
+  "target_cytoscapejs_version" : "~2.1",
+  "data" : {
+    "shared_name" : "Network",
+    "name" : "Network",
+    "SUID" : 124,
+    "__Annotations" : [ ],
+    "selected" : true
+  },
+  "elements" : {
+    "nodes" : [ {
+      "data" : {
+        "id" : "194",
+        "shared_name" : "Node 3",
+        "name" : "LOJA",
+        "SUID" : 194,
+        "selected" : true
+      },
+      "position" : {
+        "x" : -529.7968873774234,
+        "y" : -7.216631326961803
+      },
+      "selected" : true
+    }, {
+      "data" : {
+        "id" : "184",
+        "shared_name" : "Node 2",
+        "name" : "CASA",
+        "SUID" : 184,
+        "selected" : false
+      },
+      "position" : {
+        "x" : -324.7833686730382,
+        "y" : -42.767805560155274
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "179",
+        "shared_name" : "Node 1",
+        "name" : "CIDADE",
+        "SUID" : 179,
+        "selected" : false
+      },
+      "position" : {
+        "x" : -475.69526919021865,
+        "y" : -108.23344556677888
+      },
+      "selected" : false
+    } ],
+    "edges" : [ {
+      "data" : {
+        "id" : "199",
+        "source" : "179",
+        "target" : "194",
+        "shared_name" : "Node 1 (interacts with) Node 3",
+        "name" : "Node 1 (interacts with) Node 3",
+        "interaction" : "interacts with",
+        "SUID" : 199,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "189",
+        "source" : "179",
+        "target" : "184",
+        "shared_name" : "Node 1 (interacts with) Node 2",
+        "name" : "Node 1 (interacts with) Node 2",
+        "interaction" : "interacts with",
+        "SUID" : 189,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    } ]
+  }
+}}
